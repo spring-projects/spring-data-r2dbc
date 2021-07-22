e5aeae9033f0a0cd8fad26c1a4d6bed4cf741531
Mark Paluch <mpaluch@vmware.com>
2021-07-22 09:00:17 +0200

    Defer PersistentEntity lookup until actual DTO conversion.
    We now lazily instantiate DtoInstantiatingConverter to defer the entity lookup if needed.
    
    Closes #612

--- src/main/java/org/springframework/data/r2dbc/repository/query/R2dbcQueryExecution.java
+++ src/main/java/org/springframework/data/r2dbc/repository/query/R2dbcQueryExecution.java
@@ -29,6 +29,7 @@ import org.springframework.data.relational.core.sql.SqlIdentifier;
 import org.springframework.data.relational.repository.query.DtoInstantiatingConverter;
 import org.springframework.data.repository.query.ResultProcessor;
 import org.springframework.data.repository.query.ReturnedType;
+import org.springframework.data.util.Lazy;
 import org.springframework.data.util.ReflectionUtils;
 import org.springframework.r2dbc.core.FetchSpec;
 import org.springframework.util.ClassUtils;
@@ -73,6 +74,7 @@ interface R2dbcQueryExecution {
 		private final ResultProcessor processor;
 		private final MappingContext<? extends RelationalPersistentEntity<?>, ? extends RelationalPersistentProperty> mappingContext;
 		private final EntityInstantiators instantiators;
+		private final Lazy<DtoInstantiatingConverter> converter;
 
 		ResultProcessingConverter(ResultProcessor processor,
 				MappingContext<? extends RelationalPersistentEntity<?>, ? extends RelationalPersistentProperty> mappingContext,
@@ -80,6 +82,8 @@ interface R2dbcQueryExecution {
 			this.processor = processor;
 			this.mappingContext = mappingContext;
 			this.instantiators = instantiators;
+			this.converter = Lazy.of(() -> new DtoInstantiatingConverter(processor.getReturnedType().getReturnedType(),
+					this.mappingContext, this.instantiators));
 		}
 
 		/* (non-Javadoc)
@@ -109,10 +113,7 @@ interface R2dbcQueryExecution {
 				}
 			}
 
-			Converter<Object, Object> converter = new DtoInstantiatingConverter(returnedType.getReturnedType(),
-					this.mappingContext, this.instantiators);
-
-			return this.processor.processResult(source, converter);
+			return this.processor.processResult(source, it -> this.converter.get().convert(it));
 		}
 	}
 }
